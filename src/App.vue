c6c50762ad9a3050f3b1df55a73bb3ec064e57a4
RominaMalimban <romina.lyn@hotmail.it>
2022-12-12 22:08:11 +0100

    main completato

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
+
 
 export default {
   name: "App",
   components: {
     AppHeader,
+    AppMain,
     AppFooter
   }
 }
@@ -14,6 +17,7 @@ export default {
 
 <template>
   <AppHeader />
+  <AppMain />
   <AppFooter />
 </template>
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            Content goes here
+        </div>
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+main {
+    background-color: #1c1c1c;
+
+    .container {
+        color: $text-white;
+        padding: 40px 0;
+    }
+}
+</style>
