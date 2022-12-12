00a1652435c869d25fa4d2879936e07ff1e02bb6
RominaMalimban <romina.lyn@hotmail.it>
2022-12-12 17:39:46 +0100

    inserimento logo e nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader />
 </template>
 
 <style lang="scss">
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,19 +1,112 @@
 <script>
 export default {
     name: "AppHeader",
-
+    data() {
+        return {
+            logo: "img/dc-logo.png",
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false
+                },
+            ]
+        }
+    }
 }
 
 </script>
 
 <template>
-    <!-- header -->
 
-    <!-- logo -->
+    <header>
+        <div class="container">
 
-    <!-- nav bar -->
+            <a href="#">
+                <img :src="logo" alt="logo">
+            </a>
+
+            <nav>
+                <ul v-for="link in links">
+                    <li><a href="#">{{ link.text }}</a></li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
+
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
 
+header {
+    height: 95px;
+    padding: 10px 0;
+    // debug
+    border: 1px solid black;
+
+    img {
+        height: 80px;
+    }
+}
+
+.container {
+    display: flex;
+    justify-content: space-between;
+}
+
+ul {
+    list-style: none;
+    display: flex;
+
+    li a {
+        text-decoration: none;
+    }
+}
 </style>
